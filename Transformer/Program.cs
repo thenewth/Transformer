6a83e0cf595f0a3e4d11932fae8f5065816ecae5
thenewth <121046114+thenewth@users.noreply.github.com>
2025-09-25 22:26:28 +0900

    초기 커밋.

---
+++ Transformer/Program.cs
@@ -0,0 +1,14 @@
+﻿using TransformersSharp;
+
+var transformer = SentenceTransformer.FromModel("sentence-transformers/clip-ViT-B-32-multilingual-v1", trustRemoteCode: true);
+var sentences = new List<string>
+{
+    "The quick brown fox jumps over the lazy dog.",
+    "Transformers are amazing for natural language processing."
+};
+var embeddings = await transformer.GenerateAsync(sentences);
+
+foreach (var embedding in embeddings)
+{
+    Console.WriteLine($"Vector: {string.Join(", ", embedding.Vector.ToArray().Take(10))}...");
+}
